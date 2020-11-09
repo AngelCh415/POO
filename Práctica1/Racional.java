af90e262a14612e4450cbc58451b8eea621e6c65
AngelCh415 <angelchavezmorones@gmail.com>
2020-11-08 22:28:34 -0600

    Agregando la lectura del usuariov

--- Práctica1/Racional.java
+++ Práctica1/Racional.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -30,6 +32,14 @@ public class Racional {
             this.den= 1;
         else
             this.den = den;
+    }
+    public void establecer (){
+        Random numero = new Random();
+        int num =0, den = 0; 
+        num = numero.nextInt(9);
+        den = numero.nextInt(9); 
+        this.num = num;
+        this.den = den;
     }
     public void imprimir(){
         System.out.println("[" + num + ", " + den + "]");
--- Práctica1/UserRacional.java
+++ Práctica1/UserRacional.java
@@ -9,12 +9,43 @@
  * @author angelchavez
  */
 import java.util.Random;
+import java.util.Scanner;
 public class UserRacional {
 
     public static void main(String[] args) {
+        String[] operacion = new String[] {
+    		"Suma", 
+    		"Resta", 
+    		"División",
+    		"Multiplicación",
+            };
+        String [] signo = new String[]{
+            "+", "-", "/" , "*",
+        };
+        Racional A = new Racional();
+        Racional B = new Racional();
+        A.establecer();
+        B.establecer();
         int opcion = 0; 
+        int repetir = 1;
         Random azar = new Random();
-        opcion = azar.nextInt(4);
-        System.out.println("Hola :D");
-        System.out.println("La operacion ");
+        while(repetir==1){
+            System.out.flush();
+            opcion = azar.nextInt(4);
+            System.out.println("Hola :D");
+            System.out.println("La operación que harás es  "+operacion[opcion]+
+            "De estos dos números racionales ");
+            A.imprimir();
+            System.out.println(signo[opcion]);
+            B.imprimir();
+            System.out.println("Ingrese el denominador: ");
+            Scanner consola = new Scanner (System.in);
+            int den = consola.nextInt();
+            System.out.println("Ingrese el numerador: ");
+            Scanner consolas = new Scanner (System.in);
+            int num = consolas.nextInt();
+            A.establecer(den,num);
+            A.imprimir();
+        }
+    }
 }
