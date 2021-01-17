10851146564d46e19355dd04e42ff9e5362a563c
AngelCh415 <angelchavezmorones@gmail.com>
2021-01-17 16:09:07 -0600

    Arreglando archivos

---
+++ CtaAhorros.java
@@ -0,0 +1,16 @@
+public class CtaAhorros implements Impuestos{
+    private double Tasa; 
+    private int id;
+    public CtaAhorros(double saldo, double tasa ){
+
+    }
+    public Double consultar (){
+        return 0.0;
+    }
+    public void pagarISRmensual(){
+
+    }
+    public void pagarISRanual(){
+
+    }
+}
---
+++ Impuestos.java
@@ -0,0 +1,10 @@
+public interface Impuestos {
+    public double ISRm = 0.10; 
+    public double ISRa = 0.15;
+    public void pagarISRmensual(){
+
+    }
+    public void pagarISRanual(){
+        
+    }
+}
