4e1520148fa1c924d220071bc08b2c2603e09d89
AngelCh415 <angelchavezmorones@gmail.com>
2021-01-17 16:11:22 -0600

    Terminando UML en codigo

---
+++ Práctica3/CtaCheques.java
@@ -0,0 +1,13 @@
+public class CtaCheques{
+    private double sobregiro; 
+    private int id; 
+    public CtaCheques ( double saldo, double sobre){
+
+    }
+    public void depositar (double monto){
+
+    }
+    public void retirar (double monto){
+        
+    }
+}
---
+++ Práctica3/Cuenta.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+import java.util.Date;
+public class Cuenta {
+    protected double saldo;
+    private Fecha fecha;
+    public  Cuenta (String nombre) {
+
+    }
+    public Cuenta (String nombre, Fecha fecha) {
+
+    }
+    public Double consultar (){
+        return 0.0;
+    }
+    public void depositar (double monto){
+
+    }
+    public void retirar (double monto){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
