24bd5d59fef5769a47e5493eb8b4521ceeb84a6a
AngelCh415 <angelchavezmorones@gmail.com>
2021-01-17 15:45:12 -0600

    Acabando Diagrama UML

---
+++ CtaCredito.java
@@ -0,0 +1,14 @@
+public class CtaCredito {
+    private double tasa;
+    private double déficit;
+    private int id; 
+    public CtaCredito (double saldo, double tasa){
+
+    }
+    public void depositar (double monto){
+
+    }
+    public void retirar (double monto){
+        
+    }
+}
