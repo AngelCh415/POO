a669777a0b12ae6682d79bc2b9c9cfa1a90b10ea
AngelCh415 <angelchavezmorones@gmail.com>
2021-01-20 10:52:26 -0600

    Empezando a cambiar el codigo

--- Práctica3/Impuestos.java
+++ Práctica3/Impuestos.java
@@ -1,10 +1,12 @@
 public interface Impuestos {
     public double ISRm = 0.10; 
     public double ISRa = 0.15;
-    public void pagarISRmensual(){
-        
+    
+    public static void pagarISRmensual() {
+
     }
-    public void pagarISRanual(){
-        
+    
+    public static void pagarISRanual() {
+
     }
 }
