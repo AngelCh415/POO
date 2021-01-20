6b2633691a99a4ceb77ee21ee0d4a65921b059d5
AngelCh415 <angelchavezmorones@gmail.com>
2021-01-20 10:42:02 -0600

    Clonando repo

--- Práctica3/Cliente.java
+++ Práctica3/Cliente.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
-public class Cliente extends Cuenta{
+public class Cliente{
+	private String				nombre;
+	private ArrayList<Cuenta>	cuentas;
+	private String				numCliente;
 
-    private String Nombre; 
-    private ArrayList<Cuenta> cuentas; 
-    private int numCuentas;
-    private String numCuenta;
-
-    public Cliente(String nombre) {
-        
-    }
-    public Cliente (String nombre, ArrayList<Cuenta> ctas){
-
-    }
-    public void agregarCuenta (Cuenta cta){
-
-    }
-    public Cuenta obtenerCuenta (int index){
-        Cuenta a;
-        return a;
-    }
-    public void reportarEdosCtas(){
-        
-    }
-
+	public Cliente(String nom, ArrayList<Cuenta> ctas){
+		this.nombre = nom;
+		this.cuentas = ctas;
+		this.numCliente = nom.substring(0,5) +  new Random().nextInt(500) ;
+	}
+	public ArrayList<Cuenta> getCuentas(){return cuentas;}
+	public void agregarCuenta(Cuenta cta){
+		cta.setId( cuentas.size() + 1 );
+		cuentas.add(cta);
+		System.out.println("LOG: Cliente " + nombre + " tiene nueva cuenta #" + cta.getId() + " de " + (
+				( cta instanceof CtaAhorros ) ? "Ahorros" :
+				( cta instanceof CtaCheques ) ? "Cheques" :
+				( cta instanceof CtaCredito ) ? "Crédito" : "Generica"
+		));
+	}
+	public Cuenta obtenerCuenta(int index){return this.cuentas.get(index);}
+	public void reportarEdosCtas(){
+		System.out.println("Cliente: " + nombre +" No.Cuenta: " + numCliente );
+		cuentas.forEach( ( c ) -> c.consultar() );
+	}
 }
--- Práctica3/Cuenta.java
+++ Práctica3/Cuenta.java
@@ -1,25 +1,55 @@
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.Date;
-public class Cuenta {
-    protected double saldo;
-    private Fecha fecha;
-    public  Cuenta (String nombre) {
+public class Cuenta{
+	protected int id;
+	protected double saldo;
+	protected Fecha apertura;
+	protected Map<Fecha, String> movs;
 
-    }
-    public Cuenta (String nombre, Fecha fecha) {
+	public Cuenta(double saldo){
+		this( 0, Fecha.toDay(), saldo );
+	}
 
-    }
-    public Double consultar (){
-        return 0.0;
-    }
-    public void depositar (double monto){
+	public Cuenta(int id, Fecha apertura, double saldo ){
+		this.id = id;
+		this.apertura = apertura;
+		this.saldo = saldo;
+		movs = new HashMap<Fecha, String>();
+	}
+	public void consultar(){
+		regC(saldo);
+		System.out.println("\t"+this.getClass().getName() + " #" + id + " Fecha de apertura: " + apertura.toString() + " Saldo actual: $" + saldo );
+		java.util.Iterator it = movs.keySet().iterator();
+		while(it.hasNext()){
+			Fecha key = ( Fecha ) it.next();
+			System.out.println( "\t\t"+movs.get(key) + " Fecha: " + key.toString() );
+		}
+	}
+	public void depositar(double monto){
+		saldo += monto;
+		regD(monto);
+	}
+	public void retirar(double monto){
+		if( monto <= saldo ){
+			saldo -= monto;
+			regR(monto);
+		}
+	}
 
-    }
-    public void retirar (double monto){
+	public void regD(double monto){
+		movs.put( Fecha.toDay(), "Deposito: Monto: $"+monto );
+	}
 
-    }
-    public static void main(String[] args) {
-        
-    }
+	public void regR(double monto){
+		movs.put( Fecha.toDay(), "Retiro: Saldo: $"+monto );
+	}
+
+	public void regC(double monto){
+		movs.put( Fecha.toDay(), "Consulta: Saldo: $"+monto );
+	}
+
+	public void setId( int id ){ this.id = id; }
+	public int getId(){ return this.id; }
 }
